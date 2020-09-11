b8525ab87fcd48ab137e32837c83a098103bc3dd
Cl <cloudsnake@163.com>
2020-09-11 18:50:14 +0800

    add

--- TestMod/ViewModels/GridViewCustomViewModel.cs
+++ TestMod/ViewModels/GridViewCustomViewModel.cs
@@ -24,13 +24,13 @@ namespace TestMod.ViewModels
         public GridViewCustomViewModel()
         {
             var cd = new CustomData();
-            cd.ProjectInfo = new TbEntity() {Info = "测试项目"};
-            cd.ContractInfos = new TbEntity() {Info = "降低房价俄军方咖啡机骄傲的时刻房价大幅"};
+            cd.ProjectInfo = new TbEntity() {Info = "测试项目234234234234234"};
+            cd.ContractInfos = new TbEntity() {Info = "降低房价俄军方咖啡机骄傲的时刻房价大幅retwrtwertwertwertwretwerwrwer"};
             cd.TbEntity6s = new TbEntity6(){DateTime1 = DateTime.Now,Amount1 = 1254,DateTime2 = DateTime.Now,DateTime3 = DateTime.Now,Amount2 = 123,Amount3 = 8889};
            
             var cd2 = new CustomData();
-            cd2.ProjectInfo = new TbEntity() { Info = "测试项目" };
-            cd2.ContractInfos = new TbEntity() { Info = "降低房价俄军方咖啡机骄傲的时刻房价大幅" };
+            cd2.ProjectInfo = new TbEntity() { Info = "测试项目234234234234234" };
+            cd2.ContractInfos = new TbEntity() { Info = "降低房价俄军方咖啡机骄傲的时刻房价大幅retwrtwertwertwertwretwerwrwer" };
 
             cd2.TbEntity6s = new TbEntity6() { DateTime1 = DateTime.Now, Amount1 = 2222, DateTime2 = DateTime.Now, DateTime3 = DateTime.Now, Amount2 = 2222, Amount3 = 2222 };
 
